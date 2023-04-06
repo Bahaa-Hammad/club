a1047378543c204d3c7bbdde4d8f942e69e0cdbf
Bahaa Hammad <219110085@psu.edu.sa>
2023-04-06 19:29:33 +0300

    Database in container

--- ieee/club/settings.py
+++ ieee/club/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
