2b6f575fd0df25d36d8c93376566a152753a8726
Bahaa Hammad <219110085@psu.edu.sa>
2023-04-10 09:08:43 +0300

    CloudWatch

--- ieee/club/settings.py
+++ ieee/club/settings.py
@@ -157,3 +157,19 @@ MESSAGE_TAGS = {
 if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
