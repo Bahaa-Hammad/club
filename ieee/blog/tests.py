6ec09b9a640fa1e40eb22d6967d3b2cf517b9270
Bahaa Hammad <219110085@psu.edu.sa>
2023-04-11 04:00:38 +0300

    fixed static code bug

--- ieee/blog/tests.py
+++ ieee/blog/tests.py
@@ -7,8 +7,8 @@ class PostTest(TestCase):
 
     def setUp(self):
         Post.objects.create(id="9b9325b9-6ed9-46d1-9499-92939b39ed62", title="IEEE XP", content="Programming", is_published=True)
-        Post.objects.create(title="CCIS Open Day",  content="lorm", is_published=True)
-        Post.objects.create(title="Elections",  content="Elections blog", is_published=False)
+        Post.objects.create(title="CCIS Open Day", content="lorm", is_published=True)
+        Post.objects.create(title="Elections", content="Elections blog", is_published=False)
 
     def test_get_posts_exists(self):
         response = Post.get_posts()
